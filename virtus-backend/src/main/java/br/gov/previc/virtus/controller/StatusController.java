1ff5548acacea00171f3a1c98853554b051c0070
Masaru Ohashi Jr <mohashi@avaya.com>
2021-11-18 20:44:18 -0300

    MySQL working

--- virtus-backend/src/main/java/br/gov/previc/virtus/controller/StatusController.java
+++ virtus-backend/src/main/java/br/gov/previc/virtus/controller/StatusController.java
@@ -1,10 +1,14 @@
 package br.gov.previc.virtus.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,54 +16,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.gov.previc.virtus.domain.Status;
+import br.gov.previc.virtus.repository.StatusRepository;
 
-@RestController
+@Controller
 @CrossOrigin(origins = { "http://localhost:4200" })
 public class StatusController {
+    @Autowired
+    private StatusRepository statusRepository;
 
-    private Map<Integer, Status> statuses = new HashMap<Integer, Status>();
-
-    @GetMapping("/statuses")
-    public List<Status> getAll() {
-        return new ArrayList<Status>(statuses.values());
+    @GetMapping(path = "/statuses")
+    public @ResponseBody List<Status> getAll() {
+        Iterable<Status> iterable = statusRepository.findAll();
+        List<Status> result = StreamSupport.stream(iterable.spliterator(), false).collect(Collectors.toList());
+        return result;
     }
 
-    @GetMapping("/status")
-    public Status getById(int id) {
-        return statuses.get(id);
-    }
-
-    @PostMapping("/status")
-    public void add(@RequestBody Status status) {
+    @PostMapping(value = "/status")
+    public @ResponseBody Status add(@RequestBody Status status) {
         System.out.println("Adding "+status.getName());
-        int max = getMaxKey() + 1;
-        status.setId(max);
-        statuses.put(max, status);
-    }
-
-    @PutMapping("/status/{id}")
-    @CrossOrigin(origins = { "http://localhost:4200" })
-    public void update(@PathVariable int id, @RequestBody Status status) {
-        System.out.println("Updating "+id+" - "+status.getName());
-        statuses.replace(id, status);
+        return statusRepository.save(status);
     }
 
     @DeleteMapping("/status/{id}")
-    public void delete(@PathVariable int id) {
-        System.out.println("Deleting "+id);
-        statuses.remove(id);
+    public ResponseEntity<Map<String, Boolean>> delete(@PathVariable int id) {
+        Status status = statusRepository.findById(id).get();
+        statusRepository.delete(status);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);        
     }
 
-    private int getMaxKey() {
-        int aux = 0;
-        for (Status s : statuses.values()) {
-            if (aux < s.getId()) {
-                aux = s.getId();
-            }
-        }
-        return aux;
-    }
+    @PutMapping("/status/{id}")
+	public ResponseEntity<Status> update(@PathVariable Integer id, @RequestBody Status statusDetails){
+		Status status = statusRepository.findById(id).get();
+		status.setName(statusDetails.getName());
+		Status updatedStatus = statusRepository.save(status);
+		return ResponseEntity.ok(updatedStatus);
+	}
+
 }
